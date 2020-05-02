224210e88ba949acf70d961dae375081dbfd37eb
venki0922 <53636066+venki0922@users.noreply.github.com>
2020-05-02 13:33:06 +0530

    commit HelloWorld class

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,8 +6,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
-    
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    System.out.println("peinting current local time..");
+    Greeter greeter2 = new Greeter();
+    System.out.println(greeter2.sayHello());
   }
 }
